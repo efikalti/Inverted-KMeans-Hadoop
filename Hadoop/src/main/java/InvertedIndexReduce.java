177bed6ca731e98e727ad60216b330a7f2ceaf11
efikalti <efikalti@gmail.com>
2015-05-10 20:48:53 +0300

    find most important words

--- Hadoop/src/main/java/InvertedIndexReduce.java
+++ Hadoop/src/main/java/InvertedIndexReduce.java
@@ -1,5 +1,7 @@
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -10,17 +12,61 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class InvertedIndexReduce extends
         Reducer<Text, Text, Text, Text>{
     @Override
-    public void reduce(Text text, Iterable<Text> values, Context context) throws IOException, InterruptedException
+    public void reduce(Text word, Iterable<Text> values, Context context) throws IOException, InterruptedException
     {
-        String output = "[ ";
-        for (Text filename : values)
+        HashMap<String, Integer> appearences = new HashMap<String, Integer>();
+        int num;
+        String filename = "";
+        String output = "[";
+        
+        for (Text value : values)
         {
-            if (!output.contains(filename.toString()))
+            filename = value.toString();
+            //count appearences of each work in each file
+            if (!appearences.containsKey(filename))
             {
-                output = output + filename.toString() + ", ";
+                 appearences.put(filename, 1);
+            }
+            else
+            {
+                num = appearences.get(filename);
+                num++;
+                appearences.put(filename, num);
+            }
+            //add filename to inverted index if it does not exists already
+            if (!output.contains(filename))
+            {
+                output = output + filename + ",";
             }
         }
-        output = output + " ]";
-        context.write(text, new Text(output)); 
+        
+        //if this word is important it will appear a lot of times in one file but
+        //but not in other files
+        
+        //find max in values
+        boolean important = true;
+        int max = 0;
+        for (Entry<String,Integer> e : appearences.entrySet())
+        {
+            if (max < e.getValue())
+            {
+                max = e.getValue();
+                filename = e.getKey();
+            }
+        }
+        //compare max with every other
+        for (Entry<String,Integer> e : appearences.entrySet())
+        {
+            if ((max - e.getValue() == 0) || max - e.getValue() < 3)
+            {
+                if (!filename.equals(e.getKey()))
+                {
+                    important = false;
+                }
+            }
+        }
+        
+        output = output + "]";
+        context.write(word, new Text(output)); 
     }
 }
